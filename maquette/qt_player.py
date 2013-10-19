b1140e725ed8b09c78041ef5cd349d474b070110
Joël Bourgault <joel.bourgault@gmail.com>
2013-10-19 18:42:09 +0200

    Working with Qt4 Python code evolution : now having basic bookmarkings (still hard-coded) Doing a prototype under Qt4-designer
    To do:
    - connect the ui with the Python code, to make it work
    - see how bookmark data can be embedded in media files (mp3, ogg)
    - work on bookmarking processes

--- maquette/qt_player.py
+++ maquette/qt_player.py
@@ -1,139 +1,165 @@
+import bisect
+import os
 import sys
 
 import PyQt4.QtGui as QtGui
 import PyQt4.QtCore as QtCore
-from PyQt4.QtGui import QApplication, QMainWindow, QDirModel, QColumnView
-from PyQt4.QtGui import QFrame
-from PyQt4.QtCore import SIGNAL
+from PyQt4.QtGui import QMainWindow, QApplication
+#from PyQt4.QtGui import QFrame
+#from PyQt4.QtCore import SIGNAL
 from PyQt4.phonon import Phonon
 
+#==========================================================================
+# Defining constants
+#==========================================================================
+WINDOW_TITLE = "Rehearsal-player"
+
+ICON_WINDOW = '../icons/media-playback-start.png'
+
+ICON_OPEN  = '../icons/document-open.png'
+ICON_PLAY  = '../icons/media-playback-start.png'
+ICON_PAUSE = '../icons/media-playback-pause.png'
+ICON_STOP  = '../icons/media-playback-stop.png'
+
+ICON_PREV  = '../icons/go-previous.png'
+ICON_NEXT  = '../icons/go-next.png'
+
+ICON_EXIT  = '../icons/system-log-out.png'
+
+ICON_VOLUME_MUTED = '../icons/audio-volume-muted.png'
+ICON_VOLUME_MUTED = '../icons/audio-volume-muted.png'
+ICON_VOLUME_LOW = '../icons/audio-volume-low.png'
+ICON_VOLUME_MED = '../icons/audio-volume-medium.png'
+ICON_VOLUME_HIGH = '../icons/audio-volume-high.png'
+
+BOOKMARKS = [0, 13000, 46000, 100000, 210000, 230000]  # in ms
+
+#==========================================================================
+# Defining widgets
+#==========================================================================
+
 class MainWindow(QMainWindow):
-
-    m_model = QDirModel()
-
-    def __init__(self):
-        QMainWindow.__init__(self)
-        self.m_fileView = QColumnView(self)
-        self.m_media = None
-
-        self.setCentralWidget(self.m_fileView)
-        self.m_fileView.setModel(self.m_model)
-        self.m_fileView.setFrameStyle(QFrame.NoFrame)
-
-        self.connect(self.m_fileView,
-                SIGNAL("updatePreviewWidget(const QModelIndex &)"), self.play_file)
-
-    def play_file(self, index):
-        self.delayedInit()
-        self.m_media.setCurrentSource(
-                Phonon.MediaSource(self.m_model.filePath(index)))
-        self.m_media.play_file()
-
-    def delayedInit(self):
-        if not self.m_media:
-                self.m_media = Phonon.MediaObject(self)
-                audioOutput = Phonon.AudioOutput(Phonon.MusicCategory, self)
-                Phonon.createPath(self.m_media, audioOutput)
-
-def main():
-    app = QApplication(sys.argv)
-    QApplication.setApplicationName("Phonon Tutorial 2 (Python)")
-    mw = MainWindow2()
-    mw.show()
-    sys.exit(app.exec_())
-
-class MainWindow2(QMainWindow):
-    def __init__(self):
+    def __init__(self, fn):
+        
+        
         QMainWindow.__init__(self)
         
+        # creating player resource
+        self.player = Player()
+        
         # adding controls
-        self.controls = Controls()
-        self.setCentralWidget(self.controls)
+        self.sliders = Sliders(self.player)
+        self.setCentralWidget(self.sliders)
         
         # window parameters
         self.statusBar().showMessage('Ready')
-        self.setWindowIcon(QtGui.QIcon('../icons/media-playback-start.png'))        
-        self.setWindowTitle('mini-player')    
+        self.setWindowIcon(QtGui.QIcon(ICON_WINDOW))
+        self.setWindowTitle(WINDOW_TITLE)    
         
         # rendering
         self.setGeometry(300, 300, 250, 150)
         self.show()
         
-        # creating toolbar
-        playAction = QtGui.QAction(QtGui.QIcon('../icons/media-playback-start.png'), 'Play', self)
-        pauseAction = QtGui.QAction(QtGui.QIcon('../icons/media-playback-pause.png'), 'Pause', self)
-        prevAction = QtGui.QAction(QtGui.QIcon('../icons/go-previous.png'), 'Prev', self)
-        nextAction = QtGui.QAction(QtGui.QIcon('../icons/go-next.png'), 'Next', self)
-        stopAction = QtGui.QAction(QtGui.QIcon('../icons/media-playback-stop.png'), 'Stop', self)
-        exitAction = QtGui.QAction(QtGui.QIcon('../icons/system-log-out.png'), 'Exit', self)
+        # creating buttons toolbar
+        openAction  = QtGui.QAction(QtGui.QIcon(ICON_OPEN), 'Open File', self)
+        playAction  = QtGui.QAction(QtGui.QIcon(ICON_PLAY), 'Play', self)
+        pauseAction = QtGui.QAction(QtGui.QIcon(ICON_PAUSE), 'Pause', self)
+        stopAction  = QtGui.QAction(QtGui.QIcon(ICON_STOP), 'Stop', self)
+        prevAction  = QtGui.QAction(QtGui.QIcon(ICON_PREV), 'Prev', self)
+        nextAction  = QtGui.QAction(QtGui.QIcon(ICON_NEXT), 'Next', self)
+        exitAction  = QtGui.QAction(QtGui.QIcon(ICON_EXIT), 'Exit', self)
         
         self.toolbar = self.addToolBar('Tools')
+        self.toolbar.addAction(openAction)
         self.toolbar.addAction(playAction)
         self.toolbar.addAction(pauseAction)
+        self.toolbar.addAction(stopAction)
         self.toolbar.addAction(prevAction)
         self.toolbar.addAction(nextAction)
-        self.toolbar.addAction(stopAction)
         self.toolbar.addAction(exitAction)
         
-        
         # binding signals to player
-        playAction.triggered.connect(self.controls.media_player.play)
-        pauseAction.triggered.connect(self.controls.media_player.pause)
-        nextAction.triggered.connect(self.controls.media_player.ff)
-        prevAction.triggered.connect(self.controls.media_player.fr)
-        stopAction.triggered.connect(self.controls.media_player.stop)
+        openAction.triggered.connect(self.open_file)
+        playAction.triggered.connect(self.player.play)
+        pauseAction.triggered.connect(self.player.pause)
+        stopAction.triggered.connect(self.player.stop)
+        prevAction.triggered.connect(self.prev_phase)
+        nextAction.triggered.connect(self.next_phase)
         exitAction.triggered.connect(QtGui.qApp.quit)
         
-
-        
+    def open_file(self):
+        fn = QtGui.QFileDialog.getOpenFileName(self, 
+                                               'Open file', 
+                                               '/home')
+        self.player.open_file(fn)
         
     def update_status(self, t):
         self.statusBar().showMessage(str(t))
 
-class Controls(QtGui.QWidget):
-    def __init__(self):
+         
+    def prev_phase(self):
+        target_t = BOOKMARKS[bisect.bisect_left(BOOKMARKS, 
+                                                self.player.time())-1]
+        # we're stuck at current bookmark, when we want the previous
+        if target_t > 0 and abs(target_t - self.player.time()) < 1000:
+            target_t = BOOKMARKS[bisect.bisect_left(BOOKMARKS, 
+                                                    target_t)-1]
+        self.player.seek_t(target_t)    
+                
+    def next_phase(self):
+        """just seek for 20 secs forward"""
+        target_t = BOOKMARKS[bisect.bisect_left(BOOKMARKS, 
+                                                self.player.time())]
+        self.player.seek_t(target_t)    
+
+class Sliders(QtGui.QWidget):
+
+    def __init__(self, player):
         QtGui.QWidget.__init__(self)
         
-        # initializing player
-        fn = "/home/shared/musique/Maxime Le Forestier - Ne quelque part.mp3"
-        self.media_player = Player(fn)
-        self.slider = Phonon.SeekSlider(self.media_player , self)
-        #ew = self.media_player.ew
+        # adding seek slider
+        self.seek_slider = Phonon.SeekSlider(player , self)
+        self.seek_slider.setTracking(False)
        
+        # adding volume slider widget
+        self.volume_slider = Phonon.VolumeSlider(self)
+        self.volume_slider.setOrientation(2)   
+        self.volume_slider.setAudioOutput(player.audioOutput)
         
-        sld = QtGui.QSlider(QtCore.Qt.Horizontal, self)
-        self.label = QtGui.QLabel(self)
-        self.label.setPixmap(QtGui.QPixmap('../icons/audio-volume-muted.png'))
-        
+        # puting the sliders in layout
         hbox = QtGui.QHBoxLayout()
-        hbox.addWidget(self.slider)
-        #hbox.addWidget(ew)
-        hbox.addWidget(sld)
-        hbox.addWidget(self.label)
+        hbox.addWidget(self.seek_slider)
+        hbox.addWidget(self.volume_slider)
         self.setLayout(hbox)
         
-        sld.valueChanged[int].connect(self.changeValue)
-    
-    def changeValue(self, value):
-        if value == 0:
-            self.label.setPixmap(QtGui.QPixmap('../icons/audio-volume-muted.png'))
-        elif value > 0 and value <= 30:
-            self.label.setPixmap(QtGui.QPixmap('../icons/audio-volume-low.png'))
-        elif value > 30 and value < 80:
-            self.label.setPixmap(QtGui.QPixmap('../icons/audio-volume-medium.png'))
-        else:
-            self.label.setPixmap(QtGui.QPixmap('../icons/audio-volume-high.png'))
-    
+        # binding events
+        #self.seek_slider.sliderChanged.connect(self.vol_indicator)
+        
+    def vol_indicator(self, v):
+        print "volume:", v
+       
+       
+#==========================================================================
+# Defining back-end media player
+#==========================================================================
+
 class Player(Phonon.MediaObject):
-    def __init__(self, fn):
+    def __init__(self):
         Phonon.MediaObject.__init__(self)
-        audioOutput = Phonon.AudioOutput(Phonon.MusicCategory, self)
-        self.path = Phonon.createPath(self, audioOutput)
-        self.fn = fn
-        self.setCurrentSource(Phonon.MediaSource(fn))
+        self.audioOutput = Phonon.AudioOutput(Phonon.MusicCategory, self)
+        self.path = Phonon.createPath(self, self.audioOutput)
         self.tickInterval = 10
         
-        # looking for effects
+    def open_file(self, fn):
+        self.fn = fn
+        self.setCurrentSource(Phonon.MediaSource(fn))
+        self.total_time = self.totalTime()
+        if self.total_time < 0:
+            # when total time cannot be determined
+            self.total_time = 1000000000
+        
+    def set_speed(self, s):
+        # checking that speed effect is available
         effects = [e 
                    for e in Phonon.BackendCapabilities.availableAudioEffects()
                    if str(e.name())=='speed']
@@ -141,45 +167,59 @@ class Player(Phonon.MediaObject):
             print "no speed effect found"
         else:
             print "speed effect found:", effects[0].name()
-            self._activate(effects[0])
+            effect = effects[0]
         
-    def _activate(self, effect):
+        
+        # setting speed
         e = Phonon.Effect(effect)
         s = e.parameters()[0]
         
         # set speed
-        #e.setParameterValue(s, QtCore.QVariant(2.0))
+        # FIXME: no backend seems to be able to use this function
+        e.setParameterValue(s, QtCore.QVariant(0.5))
+        # insert effect in rendering path
+        self.path.insertEffect(e)
+        self.ew = Phonon.EffectWidget(e)
         
         # print current status
-        print [float(i.toString()) for i in ( e.parameterValue(s), s.minimumValue(), s.maximumValue())]
+        print [float(i.toString()) for i in ( e.parameterValue(s), 
+                                              s.minimumValue(), 
+                                              s.maximumValue())]
         print e.description().description()
         
+    def time(self):
+        return self.currentTime()
         
-        # insert effect in rendering path
-        #self.path.insertEffect(e)
-        #self.ew = Phonon.EffectWidget(e)
-        
-        
-    def ff(self):
-        """just seek for 20 secs forward"""
-        target_t = self.currentTime() + 20000
-        total_t = self.totalTime()
-        if target_t < total_t and self.isSeekable():
-            print "seekable, going to %s" % target_t
-            self.seek(target_t)
-        else:
-            self.stop()
-            
-    def fr(self):
-        """just seek for 20 secs backward"""
-        target_t = self.currentTime() - 20000
+    def seek_t(self, target_t):
         if self.isSeekable():
-            print "seekable, going to %s" % max(0, target_t)
-            self.seek(max(0, target_t))
+            if target_t < self.total_time:
+                print "seekable, going to %s" % target_t
+                self.seek(target_t)
+            else:
+                print ("target time ({0}) is more than total time ({1})"
+                       .format(target_t, self.total_time))
         else:
-            self.stop()
+            print "not seekable, ignoring seek command"
+        
+   
+
             
+#==========================================================================
+# Starting the application
+#==========================================================================
+
+def main(fn):
+    app = QApplication(sys.argv)
+    QApplication.setApplicationName("Python rehearsal player")
+    mw = MainWindow(fn)
+    mw.show()
+    sys.exit(app.exec_())
     
+#==========================================================================
+# Auto-start
+#==========================================================================
 
 if __name__ == '__main__':
-    main()
+    fn = os.path.join('..', 'sample files', 'Maxime Le Forestier - Ne quelque part.mp3')
+    main(fn)
+    
